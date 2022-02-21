f90336550c4a666723db33209d142bb39418facf
Chia Carlyle <chiacarlyle@gmail.com>
2022-02-21 16:18:25 +0100

    recreate my awesome books web application using es6-syntax and modules

---
+++ index.js
@@ -0,0 +1,6 @@
+/* eslint-disable no-unused-vars */
+import * as main from './modules/main.js';
+import * as inter from './modules/interim.js';
+import * as sections from './modules/sections.js';
+import myLibrary from './modules/interim.js';
+import displayTime from './modules/date.js';
